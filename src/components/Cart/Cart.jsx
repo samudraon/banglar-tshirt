f8b39b8e580ddbcde9c0dac7ea46ce77e242d866
samudraon <samudraon@gmailcom>
2023-04-07 12:23:33 +0600

    conditional rendering and conditional class added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please add some products</p>
+    }
+    else {
+        message = <div>
+            <h3>Boroloxx</h3>
+            <p><small>Thanks for giving your money</small></p>
+        </div>
+    }
     return (
         <div>
-            <h2>Order Summary: {cart.length}</h2>
+            <h2 className={cart.length === 1 ? 'blue' : 'red'}>Order Summary: {cart.length}</h2>
+            <p className={`bold bordered ${cart.length === 3 ? 'lightblue' : 'orange'}`}>Something</p>
+            {
+                cart.length > 2
+                    ? <span className='purple'>Aro kino</span>
+                    : <span>Fokira</span>
+            }
+            {message}
             {
                 cart.map(tshirt =>
                     <p key={tshirt._id}>
@@ -11,8 +29,29 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                         <button onClick={() => handleRemoveFromCart(tshirt._id)}>X</button>
                     </p>)
             }
+            {
+                cart.length === 2 && <p>Double bonanza!!</p>
+            }
+            {
+                cart.length === 3 || <h3>Tinta to hoilo na</h3>
+            }
         </div>
     );
 };
 
 export default Cart;
+
+/**
+ * CONDITIONAL RENDERING
+ * 1. Use if or if else to set a variable that will contain an element, components
+ * 2. ternary operator: condition ? 'for true' : 'false'
+ * 3. Logical &&: (if the condition is true then the next thing will be displayed)
+ * 4. Logical ||: (if the condition is false the next thing will be displayed)
+ * 
+*/
+
+/**
+ * CONDITIONAL CSS Class
+ * 1. use ternary
+ * 2. ternary inside template string
+*/
